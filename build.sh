9bdc125ac8c5da00f8c86e5ceaa5d5210a8884d9
Martijn Koster <mak-github@greenhills.co.uk>
2017-11-24 16:03:37 +0000

    shellcheck

--- build.sh
+++ build.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
 # See http://askubuntu.com/questions/409607/how-to-create-a-customized-ubuntu-server-iso
 
-export SOURCE=ubuntu-16.04.1-server-amd64.iso
-export TARGET=$(echo "$SOURCE" | sed -e 's/server-/server-auto-/')
+VERSION=${1:-16.04.3}
+SOURCE="ubuntu-$VERSION-server-amd64.iso"
+TARGET="$(echo "$SOURCE" | sed -e 's/server-/server-auto-/')"
 
-if [ ! -f $SOURCE ]; then
-  echo missing $SOURCE
-  exit 1
+if [ ! -f "$SOURCE" ]; then
+  URL="http://releases.ubuntu.com/$VERSION/$SOURCE"
+  echo "Downloading $URL"
+  curl --output "${SOURCE}.tmp" "$URL"
+  mv "${SOURCE}.tmp" "$SOURCE"
 fi
-echo SOURCE=$SOURCE
-echo TARGET=$TARGET
+echo "SOURCE=$SOURCE"
+echo "TARGET=$TARGET"
 
 # remove old custom iso
-sudo rm -f $TARGET
+sudo rm -f "$TARGET"
 
 # install pre-requisites
 sudo apt-get install syslinux genisoimage
@@ -26,7 +29,7 @@ sudo rm -fr iso newIso
 mkdir -p iso
 
 # mount source image and copy contents
-sudo mount -o ro,loop $SOURCE iso/
+sudo mount -o ro,loop "$SOURCE" iso/
 sudo rsync -a iso/ newIso
 sudo umount iso
 sudo rmdir iso
@@ -44,7 +47,7 @@ for f in extra-user-ssh-key extra-user-passwd extra-user-fullname extra-user-use
 # The find will warn 'File system loop detected' and return non-zero exit status on the 'ubuntu' symlink to '.'
 # To avoid that, temporarily move it out of the way
 sudo mv newIso/ubuntu .
-(cd newIso; sudo find '!' -name "md5sum.txt" '!' -path "./isolinux/*" -follow -type f -exec `which md5sum` {} \; > ../md5sum.txt)
+(cd newIso; sudo find '!' -name "md5sum.txt" '!' -path "./isolinux/*" -follow -type f -exec "$(which md5sum)" {} \; > ../md5sum.txt)
 sudo mv md5sum.txt newIso/
 sudo mv ubuntu newIso
 
@@ -54,12 +57,12 @@ sudo mkisofs -r -V "Custom Ubuntu Install CD" \
   -J -l -b isolinux/isolinux.bin \
   -c isolinux/boot.cat -no-emul-boot \
   -boot-load-size 4 -boot-info-table \
-  -o $TARGET newIso/
-ls -l $TARGET
-sudo chown $USER:$USER $TARGET
-isohybrid $TARGET
+  -o "$TARGET" newIso/
+ls -l "$TARGET"
+sudo chown "$USER:$USER" "$TARGET"
+isohybrid "$TARGET"
 
 #sleep 300
 sudo rm -fr newIso rsync.out
 
-echo created $TARGET
+echo "created $TARGET"
